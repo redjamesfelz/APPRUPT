755f102b49d47543e9b904c2ac964854be9b9d73
Red James <44889501+redjamesfelz@users.noreply.github.com>
2020-05-23 12:09:28 +1000

    Update apprupt.py

--- apprupt.py
+++ apprupt.py
@@ -14,3 +14,28 @@ if appstore == 1:
     print("You have selected Google play store")
 else:
     print("You have selected Apple Appstore")
+
+#if appstore ==1:
+#for 500 times
+#read logins.txt file
+#get username
+#get password
+#Sign in
+#https://accounts.google.com/
+#Go to
+#https://play.google.com/store/apps/details?id=<app id>
+#Click Install Button
+#//*[@id="fcxH9b"]/div[4]/c-wiz[3]/div/div[2]/div/div[1]/div/c-wiz[1]/c-wiz[1]/div/div[2]/div/div[2]/div/div[2]/div/c-wiz/c-wiz/button
+#Click Write a # REVIEW button
+#//*[@id="fcxH9b"]/div[4]/c-wiz[3]/div/div[2]/div/div[1]/div/div/div[1]/div[1]/span/button
+#Write in text box
+#//*[@id="yDmH0d"]/div[2]/div/div[2]/span/div/div[3]/textarea
+#Click 1 star
+#//*[@id="yDmH0d"]/div[2]/div/div[2]/span/div/div[3]/div[2]/div[1]/span/div[1]/div
+#Click Submit
+#//*[@id="yDmH0d"]/div[2]/div/div[2]/div[3]/div/button[2]
+#Click avatar
+#//*[@id="gb"]/div[1]/div[1]/div[1]/div[2]/div[1]/a/span
+#Click SignOut
+#//*[@id="gb_71"]
+
